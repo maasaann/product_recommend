e2c319a31a833ad66037aeddf53d9ce993ba172b
maasaann <meyutana@gmail.com>
2025-05-28 13:19:35 +0900

    2025年5月28日13時11分

--- main.py
+++ main.py
@@ -29,85 +29,85 @@ logger = logging.getLogger(ct.LOGGER_NAME)
 ############################################################
 # 初期化処理
 ############################################################
-try:
-    initialize()
-except Exception as e:
-    logger.error(f"{ct.INITIALIZE_ERROR_MESSAGE}\n{e}")
-    st.error(utils.build_error_message(ct.INITIALIZE_ERROR_MESSAGE))
-    st.stop()
+# try:
+initialize()
+# except Exception as e:
+#     logger.error(f"{ct.INITIALIZE_ERROR_MESSAGE}\n{e}")
+#     st.error(utils.build_error_message(ct.INITIALIZE_ERROR_MESSAGE))
+#     st.stop()
 
-# アプリ起動時のログ出力
-if not "initialized" in st.session_state:
-    st.session_state.initialized = True
-    logger.info(ct.APP_BOOT_MESSAGE)
-
-
-############################################################
-# 初期表示
-############################################################
-# タイトル表示
-cn.display_app_title()
-
-# AIメッセージの初期表示
-cn.display_initial_ai_message()
+# # アプリ起動時のログ出力
+# if not "initialized" in st.session_state:
+#     st.session_state.initialized = True
+#     logger.info(ct.APP_BOOT_MESSAGE)
 
 
-############################################################
-# 会話ログの表示
-############################################################
-try:
-    cn.display_conversation_log()
-except Exception as e:
-    logger.error(f"{ct.CONVERSATION_LOG_ERROR_MESSAGE}\n{e}")
-    st.error(utils.build_error_message(ct.CONVERSATION_LOG_ERROR_MESSAGE))
-    st.stop()
+# ############################################################
+# # 初期表示
+# ############################################################
+# # タイトル表示
+# cn.display_app_title()
+
+# # AIメッセージの初期表示
+# cn.display_initial_ai_message()
 
 
-############################################################
-# チャット入力の受け付け
-############################################################
-chat_message = st.chat_input(ct.CHAT_INPUT_HELPER_TEXT)
+# ############################################################
+# # 会話ログの表示
+# ############################################################
+# try:
+#     cn.display_conversation_log()
+# except Exception as e:
+#     logger.error(f"{ct.CONVERSATION_LOG_ERROR_MESSAGE}\n{e}")
+#     st.error(utils.build_error_message(ct.CONVERSATION_LOG_ERROR_MESSAGE))
+#     st.stop()
 
 
-############################################################
-# チャット送信時の処理
-############################################################
-if chat_message:
-    # ==========================================
-    # 1. ユーザーメッセージの表示
-    # ==========================================
-    logger.info({"message": chat_message})
+# ############################################################
+# # チャット入力の受け付け
+# ############################################################
+# chat_message = st.chat_input(ct.CHAT_INPUT_HELPER_TEXT)
 
-    with st.chat_message("user", avatar=ct.USER_ICON_FILE_PATH):
-        st.markdown(chat_message)
 
-    # ==========================================
-    # 2. LLMからの回答取得
-    # ==========================================
-    res_box = st.empty()
-    with st.spinner(ct.SPINNER_TEXT):
-        try:
-            result = st.session_state.retriever.invoke(chat_message)
-        except Exception as e:
-            logger.error(f"{ct.RECOMMEND_ERROR_MESSAGE}\n{e}")
-            st.error(utils.build_error_message(ct.RECOMMEND_ERROR_MESSAGE))
-            st.stop()
+# ############################################################
+# # チャット送信時の処理
+# ############################################################
+# if chat_message:
+#     # ==========================================
+#     # 1. ユーザーメッセージの表示
+#     # ==========================================
+#     logger.info({"message": chat_message})
+
+#     with st.chat_message("user", avatar=ct.USER_ICON_FILE_PATH):
+#         st.markdown(chat_message)
+
+#     # ==========================================
+#     # 2. LLMからの回答取得
+#     # ==========================================
+#     res_box = st.empty()
+#     with st.spinner(ct.SPINNER_TEXT):
+#         try:
+#             result = st.session_state.retriever.invoke(chat_message)
+#         except Exception as e:
+#             logger.error(f"{ct.RECOMMEND_ERROR_MESSAGE}\n{e}")
+#             st.error(utils.build_error_message(ct.RECOMMEND_ERROR_MESSAGE))
+#             st.stop()
     
-    # ==========================================
-    # 3. LLMからの回答表示
-    # ==========================================
-    with st.chat_message("assistant", avatar=ct.AI_ICON_FILE_PATH):
-        try:
-            cn.display_product(result)
+#     # ==========================================
+#     # 3. LLMからの回答表示
+#     # ==========================================
+#     with st.chat_message("assistant", avatar=ct.AI_ICON_FILE_PATH):
+#         try:
+#             cn.display_product(result)
             
-            logger.info({"message": result})
-        except Exception as e:
-            logger.error(f"{ct.LLM_RESPONSE_DISP_ERROR_MESSAGE}\n{e}")
-            st.error(utils.build_error_message(ct.LLM_RESPONSE_DISP_ERROR_MESSAGE))
-            st.stop()
+#             logger.info({"message": result})
+#         except Exception as e:
+#             logger.error(f"{ct.LLM_RESPONSE_DISP_ERROR_MESSAGE}\n{e}")
+#             st.error(utils.build_error_message(ct.LLM_RESPONSE_DISP_ERROR_MESSAGE))
+#             st.stop()
 
-    # ==========================================
-    # 4. 会話ログへの追加
-    # ==========================================
-    st.session_state.messages.append({"role": "user", "content": chat_message})
-    st.session_state.messages.append({"role": "assistant", "content": result})
+#     # ==========================================
+#     # 4. 会話ログへの追加
+#     # ==========================================
+#     st.session_state.messages.append({"role": "user", "content": chat_message})
+#     st.session_state.messages.append({"role": "assistant", "content": result})
